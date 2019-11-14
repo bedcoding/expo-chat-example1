9aba728376915aaf355aecbf3df75aa4323ef827
ggavi2000 <ggavi2000@naver.com>
2019-11-14 14:53:44 +0900

    ← 백엔드는 이 파일

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const server = require("http").createServer(app);
 const io = require("socket.io").listen(server);
 const port = 3000;
 
+// 백엔드는 이 파일밖에 없습니다.
 io.on("connection", socket => {
   socket.on("chat message", msg => {
     console.log(msg);
